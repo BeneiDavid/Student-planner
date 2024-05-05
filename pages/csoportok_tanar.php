dedb176f988c6197fa59a726606a7f679f10594c
BeneiDavid <104833164+BeneiDavid@users.noreply.github.com>
2024-05-05 22:23:03 +0300

    fixing calendar, starting teacher group page implementation

--- pages/csoportok_tanar.php
+++ pages/csoportok_tanar.php
@@ -2,7 +2,27 @@
 
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == 'yes')
 {
-    echo "<h1>Csoportok</h1>";
+    require 'modals/add_members_modal.php';
+    require 'modals/group_details_modal.php';
+    
+    echo '<script type="text/javascript"  src="javascript/teacher_groups.js"></script>';
+
+    echo "<h1>Csoportok</h1><br><br>";
+    
+    echo "<div class='div-with-border groups-div' id='groupsDiv'>
+
+        <div class='no-created-groups-div'>Ön még nem hozott létre csoportokat. Csoportok létrehozásához kattintson a lenti gombra! </div>
+    
+    
+        </div>
+    <br><br>
+        <div class='new-task-item monthly-new-task div-with-border clickable no-select' id='createNewGroup'>
+            <button class='add-task-button'>
+                <img class='add-task-icon' src='pictures/plus-square.svg' alt='Csoport létrehozása'>  Új csoport létrehozása
+            </button>
+        </div>
+    
+    ";
 }
 else{
     print '<p class="bg-warning text-white"><a href="index.php?page=bejelentkezes"> Ön még nem jelentkezett be, kérem jelentkezzen be itt!</a></p>';
