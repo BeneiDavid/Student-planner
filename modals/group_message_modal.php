032825bd89f031757cf244c76337260546310d9c
BeneiDavid <104833164+BeneiDavid@users.noreply.github.com>
2024-05-15 01:11:26 +0300

    added warnings for user in group messages modal

--- modals/group_message_modal.php
+++ modals/group_message_modal.php
@@ -14,17 +14,21 @@
                     <input type="search" id="messageSearchInput" class='search-input' placeholder="Keresés..."><img src='pictures/search.svg' class='search-image' data-bs-toggle="tooltip" data-bs-placement="right" title="A mezőben kereshet név és azonosító alapján is."></img>
                     <br><br>
                     <div class='student-header-div'><p>Név</p><p>Azonosító</p>
-                    <img class='info-picture' src='pictures/info.png' data-bs-toggle="tooltip" data-bs-placement="right" title="Jelölje ki a hallgatókat akiknek az üzenetet szeretné küldeni, vagy jelölje be az 'Üzenet küldése az egész csoportnak' opciót!"></img>
+                    <img class='info-picture' src='pictures/info.png' data-bs-toggle="tooltip" data-bs-placement="right" title="Jelölje be a hallgatók melletti négyzetet az üzenetet elküldéséhez, vagy jelölje be az 'Üzenet küldése az egész csoportnak' opciót!"></img>
                 </div>
                     <div class='div-with-border search-results' id="searchStudentResults"></div>
-                    <span class="error no-result-text" id='noSearchResultText'>Nincs a keresésnek megfelelő találat.</span><br>
-                    <span class="info no-result-text" id='noRegisteredStudents'>A csoportnak még nincsenek tagjai.</span>
+                    <span class="error no-result-text" id='noSearchResultText'><br>Nincs a keresésnek megfelelő találat.</span><br>
+                    <span class="info no-result-text" id='noRegisteredStudents'><br>A csoportnak még nincsenek tagjai.</span>
+                    <span class="error no-result-text" id='noStudentsSelected'>Kérem adja meg, hogy kinek szeretné küldeni az üzenetet!</span>
+                    
                     <br>
                     <input type="checkbox" id="sendToAllCheckbox" name="sendToAllCheckbox">
                     <label for="sendToAllCheckbox">Üzenet küldése az egész csoportnak</label>
                     <br><br>
                     <label for='groupMessageText'>Az üzenet szövege:</label><br>
                     <textarea rows='10' class='container-fluid form-control' id="groupMessageText"></textarea>
+                    <span class="error no-result-text" id='noMessageSpecified'>Nem küldhet el üres üzenetet!</span>
+                    
                 </form>
             </div>
 
