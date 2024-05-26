3158aae5aec9660591b69e3ae4cec7dab7f0332a
BeneiDavid <104833164+BeneiDavid@users.noreply.github.com>
2024-05-26 03:21:01 +0300

    changing email configuration

--- emailconfig/emailconfig.php
+++ emailconfig/emailconfig.php
@@ -4,15 +4,15 @@
     
     define('MAILHOST', "smtp.gmail.com");
 
-    define('USERNAME', "beneidavid.it@gmail.com");
+    define('USERNAME', "studentplannermail@gmail.com");
 
-    define('PASSWORD', "zbsi pkui evtl cazw");
+    define('PASSWORD', "quax wkee wnis ouds");
 
-    define('SEND_FROM', "beneidavid.it@gmail.com");
+    define('SEND_FROM', "studentplannermail@gmail.com");
 
     define('SEND_FROM_NAME', "Student planner");
 
-    define('REPLY_TO', "beneidavid.it@gmail.com");
+    define('REPLY_TO', "studentplannermail@gmail.com");
 
     define('REPLY_TO_NAME', "Student planner")
 
